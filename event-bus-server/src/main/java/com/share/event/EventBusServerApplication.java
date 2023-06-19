f196fefbf6307551642eb27665b87d302916b039
dengyangjie <yangjie.deng@resico.cn>
2023-06-19 18:24:33 +0800

    new project

---
+++ event-bus-server/src/main/java/com/share/event/EventBusServerApplication.java
@@ -0,0 +1,13 @@
+package com.share.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBusServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventBusServerApplication.class, args);
+    }
+
+}
