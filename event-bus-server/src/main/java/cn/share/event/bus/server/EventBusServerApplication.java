e8a3363f46f830daa98fc4f80da6e1a10a710c03
gonz <zheng.gong@resico.com>
2023-06-20 08:47:20 +0800

    更新项目介绍

--- event-bus-server/src/main/java/cn/share/event/bus/server/EventBusServerApplication.java
+++ event-bus-server/src/main/java/cn/share/event/bus/server/EventBusServerApplication.java
@@ -1,4 +1,4 @@
-package com.share.event;
+package cn.share.event.bus.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
