04bfbb543d99ebfa09462ea7b69aba54ca6af1b2
gonz <zheng.gong@resico.com>
2023-06-21 14:14:26 +0800

    生产节点和生产线 基础完善

--- event-bus-server/src/main/java/cn/share/event/bus/server/domain/valueObj/ProductionNodeOutput.java
+++ event-bus-server/src/main/java/cn/share/event/bus/server/domain/valueObj/ProductionNodeOutput.java
@@ -7,4 +7,29 @@ package cn.share.event.bus.server.domain.valueObj;
  * @date 2023/6/20 9:41
  */
 public class ProductionNodeOutput {
+    /**
+     * 业务线id
+     */
+    private Long productionLineId;
+
+    /**
+     * 业务节点id
+     */
+    private Long productionNodeId;
+
+    /**
+     * 业务线实例id
+     */
+    private Long pLInstanceId;
+
+    /**
+     * 功能数据规范
+     */
+    private String dataSpecification;
+
+    /**
+     * 功能数据
+     */
+    private String data;
+
 }
